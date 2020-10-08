7b6a45aee74d3e7bfc70582ffd51c142e7ea0ddb
Jason Del Ponte <delpontej@gmail.com>
2020-10-08 11:39:04 -0700

    add presign url static middleware

---
+++ service/internal/presigned-url/middleware_test.go
@@ -0,0 +1,14 @@
+package presignedurl
+
+import "testing"
+
+func TestPresignMiddleware(t *testing.T) {
+	cases := map[string]struct {
+	}{}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Errorf("not implemented, %v", c)
+		})
+	}
+}
